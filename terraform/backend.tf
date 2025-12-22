fadcbdd5ca347be0cf8c87dad85f43febaa25324
IanTEChiou <adszxc12358@gmail.com>
2025-12-22 01:06:30 +0000

    fix format issue

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "ian-tfstate-eu-west-2"
-    key            = "terraform-admin/terraform.tfstate"
-    region         = "eu-west-2"
+    bucket = "ian-tfstate-eu-west-2"
+    key    = "terraform-admin/terraform.tfstate"
+    region = "eu-west-2"
 
-    encrypt        = true
+    encrypt = true
   }
 }
